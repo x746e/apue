976afc51f22d5153aa86bc095c9e0bf3369d6a29
Kirill Spitsyn <kyrylo@fb.com>
2015-08-01 19:03:46 -0700

    Restructured code for chapter 4.

--- common.mk
+++ common.mk
@@ -1,12 +1,12 @@
 all: $(PROGS) .gitignore
 
 %: %.c
-	cc -std=c99 -g $@.c -o $@ -lm -I ../.. ../../apue.3e/lib/error.c
+	cc -std=c99 -g $@.c -o $@ -lm -I ../.. ../../apue.3e/lib/error.c $(cfiles)
 
 .gitignore: $(PROGS)
 	touch .gitignore
 	for prog in $(PROGS); do \
-	    if ! grep -sq $$prog .gitignore; then \
+	    if ! grep -sq ^$$prog .gitignore$ ; then \
 		echo $$prog >> .gitignore ; \
 	    fi \
 	done
