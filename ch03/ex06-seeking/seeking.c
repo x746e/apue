c0377bc73bd94157ff51495889e45e846ec775a8
Kirill Spitsyn <kyrylo@fb.com>
2015-08-12 19:34:52 -0700

    Refactored ex03.06 a bit.

--- ch03/ex06-seeking/seeking.c
+++ ch03/ex06-seeking/seeking.c
@@ -4,39 +4,50 @@
  * existing data in the file?  Write a program to verify this.
  */
 #include "apue.h"
+#include "common.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 
 void check_position(int fd);
+void check_contents(int fd);
+
+// Try reading from different places.
+// Try writing in the middle
+
+char testfile[] = "testfile";
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        err_quit("Usage: %s <file>", argv[0]);
-    }
-        
     int fd;
-    if ((fd = open(argv[1], O_RDWR /*| O_APPEND*/)) < 0) {
-        err_sys("Can't open file `%s`", argv[1]);
+
+    // Create file with a..z letters.
+    sys_chk((fd = open(testfile, O_RDWR | O_CREAT | O_TRUNC, FILE_MODE)));
+    for (char c = 'a'; c <= 'z'; c++) {
+        sys_chk(write(fd, &c, 1));
     }
 
+    check_contents(fd);
+    sys_chk(close(fd));
+
+    // Open it read-write with the append flag.
+    sys_chk(fd = open(testfile, O_RDWR | O_APPEND));
     check_position(fd);
 
-    lseek(fd, 100, SEEK_SET);
+    printf("Setting position in the middle of the file\n");
+    sys_chk(lseek(fd, 10, SEEK_SET));
+    check_position(fd);
 
     char msg[] = "hello there";
-    check_position(fd);
+    printf("Writing %zd symbols\n", strlen(msg));
     write(fd, msg, strlen(msg));
     check_position(fd);
 
-    char buffer[BUFSIZ] = { 0 };
-    if (read(fd, buffer, BUFSIZ) < 0) {
-        err_sys("Can't read from fd#%d", fd);
-    }
-
+    check_contents(fd);
     check_position(fd);
-    printf("%s\n", buffer);
+
+    sys_chk(close(fd))
 }
 
 
@@ -47,4 +58,16 @@ void check_position(int fd) {
     } else {
         printf("current_position: %zd\n", current_position);
     }
+}
+
+void check_contents(int fd) {
+    char buf[BUFSIZ] = { 0 };
+    off_t saved_position;
+    sys_chk(saved_position = lseek(fd, 0, SEEK_CUR));
+    sys_chk(lseek(fd, 0, SEEK_SET));
+
+    read(fd, buf, BUFSIZ);
+    printf("File contents (%zd bytes): %s\n", strlen(buf), buf);
+
+    sys_chk(lseek(fd, saved_position, SEEK_SET));
 }
