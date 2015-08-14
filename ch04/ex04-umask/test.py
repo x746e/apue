a412951452692367764f50dfdec883555d236831
Kirill Spitsyn <kyrylo@fb.com>
2015-08-14 09:24:12 -0700

    Made test for ex04.04 running on solaris.

--- ch04/ex04-umask/test.py
+++ ch04/ex04-umask/test.py
@@ -4,7 +4,44 @@ import os
 import re
 import sys
 from pprint import pprint
-from subprocess import Popen, check_output, STDOUT, check_call
+
+from subprocess import STDOUT
+try:
+    from subprocess import check_output
+except ImportError:
+    from subprocess import Popen, CalledProcessError, PIPE
+    # Copied from python2.7 stdlib
+    def check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+
+        If the exit code was non-zero it raises a CalledProcessError.  The
+        CalledProcessError object will have the return code in the returncode
+        attribute and output in the output attribute.
+
+        The arguments are the same as for the Popen constructor.  Example:
+
+        >>> check_output(["ls", "-l", "/dev/null"])
+        'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+        The stdout argument is not allowed as it is used internally.
+        To capture standard error in the result, use stderr=STDOUT.
+
+        >>> check_output(["/bin/sh", "-c",
+        ...               "ls -l non_existent_file ; exit 0"],
+        ...              stderr=STDOUT)
+        'ls: non_existent_file: No such file or directory\n'
+        """
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = Popen(stdout=PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise CalledProcessError(retcode, cmd, output=output)
+        return output
 
 
 def test(doctest, debug=False):
@@ -77,7 +114,7 @@ test(
 ...
 % ./test-umask
 % ls -l foo bar
--rw------- 1 tn tn 0 ... bar
--rw-rw-rw- 1 tn tn 0 ... foo
+-rw------- 1 tn ... bar
+-rw-rw-rw- 1 tn ... foo
 """, debug=False,
 )
