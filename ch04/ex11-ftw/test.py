37a208e4679e5a4d06f9d58de8397e958ac2b089
Kirill Spitsyn <kyrylo@fb.com>
2015-08-15 11:33:53 -0700

    [ex04.11] Changed test not to wait for predetermined number of files, but to compare output of two ftw-s.

--- ch04/ex11-ftw/test.py
+++ ch04/ex11-ftw/test.py
@@ -2,24 +2,13 @@ from shell_doctest import tests, test
 
 test(
 """\
-% {make} -s -C .. clean
 % {make} --no-print-directory clean all
 ...
-% ./ftw ..
-regular files  =      37, 75.51 %
-directories    =       9, 18.37 %
-block special  =       0,  0.00 %
-char special   =       0,  0.00 %
-FIFOs          =       0,  0.00 %
-symbolic links =       3,  6.12 %
-sockets        =       0,  0.00 %
-% ./apue-ftw ..
-regular files  =      37, 75.51 %
-directories    =       9, 18.37 %
-block special  =       0,  0.00 %
-char special   =       0,  0.00 %
-FIFOs          =       0,  0.00 %
-symbolic links =       3,  6.12 %
-sockets        =       0,  0.00 %
+% rm output.ftw output.apue-ftw
+% touch output.ftw output.apue-ftw
+% sudo ./ftw / >| output.ftw
+% sudo ./apue-ftw / >| output.apue-ftw
+% diff output.ftw output.apue-ftw
+% rm output.ftw output.apue-ftw
 """
 )
