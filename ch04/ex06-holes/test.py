f409ba5f430fd762d05fd36ff4bc2f7ecb40a67e
Kirill Spitsyn <kyrylo@fb.com>
2015-08-15 04:12:46 -0700

    [ex04.06] More tests.

--- ch04/ex06-holes/test.py
+++ ch04/ex06-holes/test.py
@@ -9,11 +9,24 @@ tests([
 """,
 # make-file-with-holes should create file with correct number & size of holes.
 """\
-% {make} --no-print-directory clean all
-...
 % ./make-file-with-holes file.hole
 % ./check-holes file.hole
 Hole from 0 to 131072
 Hole from 262144 to 393216
+""",
+# Check that cp doesn't preserve holes.
+"""\
+% cp file.hole file.hole.cp_copy
+% ./check-holes file.hole.cp_copy
+% rm file.hole.cp_copy
+""",
+# Check that ./copy-holes preserve holes.
+"""\
+% ./copy-holes file.hole file.hole.copy
+% ./check-holes file.hole.copy
+Hole from 0 to 131072
+Hole from 262144 to 393216
+% rm file.hole.copy
 """
-], exclude=('DragonFly',))
+], exclude=('DragonFly',) # At least when using HAMMER fs.
+)
