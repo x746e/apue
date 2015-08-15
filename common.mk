44c124edc59f3d066ee102e9ec37feca16d5c4c9
Kirill Spitsyn <kyrylo@fb.com>
2015-08-14 20:40:04 -0700

    Moved `test` rule to common.mk.

--- common.mk
+++ common.mk
@@ -2,11 +2,16 @@ all: $(PROGS) .gitignore
 
 CFLAGS = -std=c99 -g -Wall
 INCLUDES = -I ../..
+# Hack to overcome deficiency of `which` on solaris.
+PYTHON = $(shell which python2.7 > /dev/null && which python2.7 || which python)
 
 CFILES += ../../apue.3e/lib/error.c 
 
 %: %.c
 	cc $(CFLAGS) $(INCLUDES) $(CFILES) $(CFILES_$@) $< -o $@ $(LDFLAGS) $(LDFLAGS_$@)
+
+test:
+	PYTHONPATH=../.. $(PYTHON) test.py
 
 .gitignore: $(PROGS)
 	touch .gitignore
