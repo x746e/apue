cad6a06d94314894f9b72cc35bbdd1820f5d404a
Kirill Spitsyn <kyrylo@fb.com>
2015-08-14 20:45:28 -0700

    [ex04.01] Added test.

---
+++ ch04/ex01-filetype/test.py
@@ -0,0 +1,22 @@
+from shell_doctest import test
+
+test(
+"""\
+% {make} --no-print-directory clean all
+...
+% ./filetype ../*
+../4.3: regular
+../4.7: regular
+../broken: stat error: No such file or directory
+../dir3: directory
+../ex01-filetype: directory
+../ex04-umask: directory
+../ex06-holes: directory
+../ex09: directory
+../ex11-ftw: directory
+../ex16-deep-tree: directory
+../readdir: directory
+../root: directory
+../usr-local: directory
+""", debug=False,
+)
