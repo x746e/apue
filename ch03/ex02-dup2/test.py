6130f658b25ea409135eec7cbdf17cfa0c2d38be
Kirill Spitsyn <kyrylo@fb.com>
2015-08-14 20:57:15 -0700

    [ex03.02] Added test.

---
+++ ch03/ex02-dup2/test.py
@@ -0,0 +1,20 @@
+from shell_doctest import test
+
+test(
+"""\
+% rm -f foo bar
+% {make} --no-print-directory clean all
+...
+% ./mydup2 mydup2.c | sed 's/\x1b\[[0-9;]*m//g'
+>> Checking fd#3
+current_position: 1651
+FD_CLOEXEC: 0
+File status: O_RDONLY
+...
+>> Checking fd#42
+current_position: 1651
+FD_CLOEXEC: 0
+File status: O_RDONLY
+...
+""", debug=False,
+)
