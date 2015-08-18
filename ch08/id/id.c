ea7780cb5028a0bd3bdc0ff4aaca0f8dfb7fd707
Kirill Spitsyn <kyrylo@fb.com>
2015-08-18 14:58:55 -0700

    [ch08/id] Print saved group/user ids.

--- ch08/id/id.c
+++ ch08/id/id.c
@@ -24,39 +24,62 @@ int main() {
     struct passwd *pwent;
     struct group *grent;
 
+    /** Different process ids */
     printf("Process ID: %d\n", getpid());
     printf("Parent PID: %d\n", getppid());
     printf("Process group ID: %d\n", getpgrp());
     printf("Session ID: %d\n", getsid(getpid()));
     
 
-    uid_t uid = getuid();
-    sys_ptr_chk(pwent = getpwuid(uid));
+    /** Different user ids */
+    uid_t real_uid = getuid();
+    sys_ptr_chk(pwent = getpwuid(real_uid));
     char *real_uname = strdupa(pwent->pw_name);
 
-    uid_t euid = geteuid();
-    sys_ptr_chk(pwent = getpwuid(euid));
+    uid_t effective_uid = geteuid();
+    sys_ptr_chk(pwent = getpwuid(effective_uid));
     char *effective_uname = strdupa(pwent->pw_name);
 
     printf("\n");
-    printf("Real user ID: %d (%s)\n", uid, real_uname);
-    printf("Effective user ID: %d (%s)\n", euid, effective_uname);
+    printf("Real user ID: %d (%s)\n", real_uid, real_uname);
+    printf("Effective user ID: %d (%s)\n", effective_uid, effective_uname);
+
+    uid_t _u1, _u2, saved_uid;
+    sys_chk(getresuid(&_u1, &_u2, &saved_uid));
+    sys_ptr_chk(pwent = getpwuid(saved_uid));
+    char *saved_uname = strdupa(pwent->pw_name);
+    printf("Saved user ID: %d (%s)\n", saved_uid, saved_uname);
 
 
-    gid_t gid = getgid();
-    sys_ptr_chk(grent = getgrgid(gid));
+    /** Different group ids */
+    gid_t real_gid = getgid();
+    sys_ptr_chk(grent = getgrgid(real_gid));
     char *real_gname = strdupa(grent->gr_name);
 
-    gid_t egid = getegid();
-    sys_ptr_chk(grent = getgrgid(egid));
+    gid_t effective_gid = getegid();
+    sys_ptr_chk(grent = getgrgid(effective_gid));
     char *effective_gname = strdupa(grent->gr_name);
 
     printf("\n");
-    printf("Real group ID: %d (%s)\n", gid, real_gname);
-    printf("Effective group ID: %d (%s)\n", egid, effective_gname);
+    printf("Real group ID: %d (%s)\n", real_gid, real_gname);
+    printf("Effective group ID: %d (%s)\n", effective_gid, effective_gname);
+
+    gid_t _g1, _g2, saved_gid;
+    sys_chk(getresgid(&_g1, &_g2, &saved_gid));
+    sys_ptr_chk(grent = getgrgid(saved_gid));
+    char *saved_gname = strdupa(grent->gr_name);
+    printf("Saved user ID: %d (%s)\n", saved_gid, saved_gname);
+
+
+    /** else */
+    printf("\n");
+    if (issetugid()) {
+        printf("Process is tainted\n");
+    } else {
+        printf("Process is *not* tainted\n");
+    }
 
     return EXIT_SUCCESS;
 }
 
-// issetugid
 // getresuid
