85fa658643a4f1bacc54d171551a7c213d1cc37e
Kirill Spitsyn <kyrylo@fb.com>
2015-08-18 14:21:14 -0700

    [ch08/id] Added simple program to check for POSIX saved ids feature.

---
+++ ch08/id/has_posix_saved_ids.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+bool is_supported(int sysconf_option);
+
+
+int main() {
+    printf("_POSIX_SAVED_IDS: ");
+#ifdef _POSIX_SAVED_IDS
+    printf("defined\n");
+#else
+    printf("not defined\n");
+#endif
+    printf("Runtime check: %s\n", is_supported(_SC_SAVED_IDS) ? "supported" : "not supported");
+    return EXIT_SUCCESS;
+}
+
+
+bool is_supported(int sysconf_option) {
+    errno = 0;
+    int result;
+    if ((result = sysconf(sysconf_option)) == -1) {
+        if (errno == 0) {
+            // `name` is not supported
+            return false;
+        }
+    }
+    printf("sysconf returned %d\n", result);
+    return result;
+}
