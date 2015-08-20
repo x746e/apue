148166d9e32649091bfcd99ccdd649723b8ae85d
Kirill Spitsyn <kyrylo@fb.com>
2015-08-20 08:59:51 -0700

    [ch08/id] Got saved set-user-id on sunos.

--- ch08/id/print_ids.c
+++ ch08/id/print_ids.c
@@ -1,18 +1,22 @@
 #define _GNU_SOURCE
+#define __EXTENSIONS__
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <pwd.h>
-#include <grp.h>
 #include <string.h>
+#include <limits.h>
 #ifdef __sun__
 #include <alloca.h>
+#include <sys/signal.h>
+#include <sys/procfs.h>
 #endif
 
 #include "apue.h"
 #include "common.h"
 
-#ifndef __linux__
+#if !defined(__linux__) && !defined(strdupa)
 #define strdupa(__str) \
     strncpy(alloca(strlen(__str) + 1), \
             __str, \
@@ -30,60 +34,73 @@
 #endif
 
 
-int print_ids() {
+#ifdef has_getresuid
+void get_user_ids(uid_t *real, uid_t *effective, uid_t *saved) {
+    sys_chk(getresuid(real, effective, saved));
+}
+#else
+#if defined(__sun__)
+void get_user_ids(uid_t *real, uid_t *effective, uid_t *saved) {
+    char cred_file_path[PATH_MAX];
+    struct prcred cred;
+
+    // TODO: error-checking snprintf macros
+    int n;
+    n = snprintf(cred_file_path, PATH_MAX, "/proc/%lu/cred", (long)getpid());
+    if (n >= PATH_MAX) {
+        err_quit("snprintf overflowed the buffer");
+    } else if (n < 0) {
+        err_sys("snprintf");
+    }
+
+    int fd;
+    sys_chk(fd = open(cred_file_path, O_RDONLY));
+
+    sys_chk(read(fd, &cred, sizeof cred));
+
+    *effective = cred.pr_euid;
+    *real = cred.pr_ruid;
+    *saved = cred.pr_suid;
+}
+#else
+void get_user_ids(uid_t *real, uid_t *effective, uid_t *saved) {
+    *real = getuid();
+    *effective = geteuid();
+    *saved = -1;
+}
+#endif
+#endif
+
+
+void print_ids() {
     struct passwd *pwent;
-    struct group *grent;
 
     /** Different process ids */
-    printf("Process ID: %d\n", getpid());
-    printf("Parent PID: %d\n", getppid());
-    printf("Process group ID: %d\n", getpgrp());
-    printf("Session ID: %d\n", getsid(getpid()));
+    printf("Process ID: %lld\n", (long long)getpid());
+    printf("Parent PID: %lld\n", (long long)getppid());
+    printf("Process group ID: %lld\n", (long long)getpgrp());
+    printf("Session ID: %lld\n", (long long)getsid(getpid()));
     
 
     /** Different user ids */
-    uid_t real_uid = getuid();
+    uid_t real_uid, effective_uid, saved_uid;
+    get_user_ids(&real_uid, &effective_uid, &saved_uid);
+
     // TODO: To distinguish between nonexistend user and error we need to set `errno=0` beforehand.
     sys_ptr_chk(pwent = getpwuid(real_uid));
     char *real_uname = strdupa(pwent->pw_name);
 
-    uid_t effective_uid = geteuid();
     sys_ptr_chk(pwent = getpwuid(effective_uid));
     char *effective_uname = strdupa(pwent->pw_name);
+
+    // TODO: Handle case when we can't get saved uid.
+    sys_ptr_chk(pwent = getpwuid(saved_uid));
+    char *saved_uname = strdupa(pwent->pw_name);
 
     printf("\n");
     printf("Real user ID: %d (%s)\n", real_uid, real_uname);
     printf("Effective user ID: %d (%s)\n", effective_uid, effective_uname);
-
-#ifdef has_getresuid
-    uid_t _u1, _u2, saved_uid;
-    sys_chk(getresuid(&_u1, &_u2, &saved_uid));
-    sys_ptr_chk(pwent = getpwuid(saved_uid));
-    char *saved_uname = strdupa(pwent->pw_name);
     printf("Saved user ID: %d (%s)\n", saved_uid, saved_uname);
-#endif
-
-
-    /** Different group ids */
-    gid_t real_gid = getgid();
-    sys_ptr_chk(grent = getgrgid(real_gid));
-    char *real_gname = strdupa(grent->gr_name);
-
-    gid_t effective_gid = getegid();
-    sys_ptr_chk(grent = getgrgid(effective_gid));
-    char *effective_gname = strdupa(grent->gr_name);
-
-    printf("\n");
-    printf("Real group ID: %d (%s)\n", real_gid, real_gname);
-    printf("Effective group ID: %d (%s)\n", effective_gid, effective_gname);
-
-#ifdef has_getresuid
-    gid_t _g1, _g2, saved_gid;
-    sys_chk(getresgid(&_g1, &_g2, &saved_gid));
-    sys_ptr_chk(grent = getgrgid(saved_gid));
-    char *saved_gname = strdupa(grent->gr_name);
-    printf("Saved user ID: %d (%s)\n", saved_gid, saved_gname);
-#endif
 
 
     /** else */
@@ -95,6 +112,10 @@ int print_ids() {
         printf("Process is *not* tainted\n");
     }
 #endif
+}
+
+int main() {
+    print_ids();
 
     return EXIT_SUCCESS;
 }
