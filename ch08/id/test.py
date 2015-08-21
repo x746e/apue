29d0efe89fc373a84638b6e45b1b4ebc9ed2c3d3
Kirill Spitsyn <kyrylo@fb.com>
2015-08-20 18:46:15 -0700

    [ch08/id] Added tests for setuid.

--- ch08/id/test.py
+++ ch08/id/test.py
@@ -7,18 +7,40 @@ tests([
 ...
 """,
 # Run as user "daemon".
-# uid is `10...` because sometimes it's 1000 (linux), sometimes 1001 (fbsd),
-# sometimes 101 (sunos).
 """\
 % sudo ./check_setuid 1
 Calling setuid(1)
-Process ID: ...
-Parent PID: ...
-Process group ID: ...
-Session ID: ...
+...
 
 Real user ID: 1 (daemon)
 Effective user ID: 1 (daemon)
+Saved user ID: 1 (daemon)
+...
+""",
+# Make file setuid daemon
+"""\
+% sudo chown daemon check_setuid
+% sudo chmod u+s check_setuid
+""",
+# Now setuid to daemon shoudn't change anything
+"""\
+% ./check_setuid 1
+Calling setuid(1)
+...
+
+Real user ID: 10... (tn)
+Effective user ID: 1 (daemon)
+Saved user ID: 1 (daemon)
+...
+""",
+# And setuid to $UID should leave saved suid as daemon
+"""\
+% ./check_setuid $(id -ur)
+Calling setuid(101)
+...
+
+Real user ID: 10... (tn)
+Effective user ID: 10... (tn)
 Saved user ID: 1 (daemon)
 ...
 """
