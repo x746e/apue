ee8c0051a9d722406a0a0b8196609a3312ff0476
Kirill Spitsyn <kyrylo@fb.com>
2015-11-11 21:45:03 -0800

    [ex15.16] `union semun` is already defined on BSD-s

--- ch15/ex16-xsi-semaphores/devzero.c
+++ ch15/ex16-xsi-semaphores/devzero.c
@@ -12,11 +12,13 @@
 #define SEM_CHILD   1
 
 
+#if !(defined(__FreeBSD__) || defined(__DragonFly__))
 union semun {
     int              val;    /* Value for SETVAL */
     struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
     unsigned short  *array;  /* Array for GETALL, SETALL */
 };
+#endif
 
 
 static int
