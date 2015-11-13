696df8fa4b46718300e95aa52c9e55e0c3972534
Kirill Spitsyn <kyrylo@fb.com>
2015-11-12 17:14:14 -0800

    [ex16.02] Print mode in octal.

--- ch16/ex02-socket-stat/socket-stat.c
+++ ch16/ex02-socket-stat/socket-stat.c
@@ -16,16 +16,16 @@ int main() {
     struct stat buf;
     sys_chk(fstat(s, &buf));
 
-    printf("ID of device containing file: %20lld\n",       (long long)buf.st_dev);         
-    printf("inode number: %20lld\n",                       (long long)buf.st_ino);         
-    printf("protection: %lld\n",                         (long long)buf.st_mode);        
-    printf("number of hard links: %lld\n",               (long long)buf.st_nlink);       
-    printf("user ID of owner: %lld\n",                   (long long)buf.st_uid);         
-    printf("group ID of owner: %lld\n",                  (long long)buf.st_gid);         
-    printf("device ID (if special file): %lld\n",        (long long)buf.st_rdev);        
-    printf("total size, in bytes: %lld\n",               (long long)buf.st_size);        
-    printf("blocksize for filesystem I/O: %lld\n",       (long long)buf.st_blksize);     
-    printf("number of 512B blocks allocated %lld\n",     (long long)buf.st_blocks);      
+    printf("ID of device containing file: %lld\n",       (long long)buf.st_dev);
+    printf("inode number: %lld\n",                       (long long)buf.st_ino);
+    printf("protection: %o\n",                           buf.st_mode);
+    printf("number of hard links: %lld\n",               (long long)buf.st_nlink);
+    printf("user ID of owner: %lld\n",                   (long long)buf.st_uid);
+    printf("group ID of owner: %lld\n",                  (long long)buf.st_gid);
+    printf("device ID (if special file): %lld\n",        (long long)buf.st_rdev);
+    printf("total size, in bytes: %lld\n",               (long long)buf.st_size);
+    printf("blocksize for filesystem I/O: %lld\n",       (long long)buf.st_blksize);
+    printf("number of 512B blocks allocated %lld\n",     (long long)buf.st_blocks);
 
 
     return EXIT_SUCCESS;
