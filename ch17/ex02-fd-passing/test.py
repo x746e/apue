cb9e70fcdf7895aca2cc9022e80d779a7befaf97
Kirill Spitsyn <kyrylo@fb.com>
2015-11-29 03:28:50 -0800

    [ex17.02] Fixed test to not hardcode passed fd number (it's different on sunos).

--- ch17/ex02-fd-passing/test.py
+++ ch17/ex02-fd-passing/test.py
@@ -5,7 +5,7 @@ test(
 % {make} --no-print-directory clean all
 ...
 % ./fd-passing
-Parent: got fd: 5, seek: 0
+Parent: got fd: ..., seek: 0
 Parent: seek after child changed it: 10 (should be 10)
 Parent: seek after child changed it second time: 20 (should be 20)
 """
