b65dacf6e9d0500564e8441f96c3793037e79e0a
Kirill Spitsyn <kyrylo@fb.com>
2015-11-29 15:39:12 -0800

    [ex19.03] Done.

--- ch19/ex03-pty-select/loop.c
+++ ch19/ex03-pty-select/loop.c
@@ -1,9 +1,9 @@
 #include "apue.h"
+#include "common.h"
+#include <sys/select.h>
 
 #define BUFFSIZE    512
-
-static void sig_term(int);
-static volatile sig_atomic_t    sigcaught;  /* set by signal handler */
+#define MAX(a, b)   ((a) > (b) ? (a) : (b))
 
 void
 loop(int ptym, int ignoreeof)
@@ -12,59 +12,30 @@ loop(int ptym, int ignoreeof)
     int     nread;
     char    buf[BUFFSIZE];
 
-    if ((child = fork()) < 0) {
-        err_sys("fork error");
-    } else if (child == 0) {    /* child copies stdin to ptym */
-        for ( ; ; ) {
+    fd_set  watch_for_set, changed_set;
+    int     nfds;  // highest-numbered file descriptor in the sets, plus 1
+    nfds = MAX(ptym, STDIN_FILENO) + 1;
+
+    FD_ZERO(&watch_for_set);
+    FD_SET(ptym, &watch_for_set);
+    FD_SET(STDIN_FILENO, &watch_for_set);
+
+    for (;;) {
+        changed_set = watch_for_set;
+        sys_chk(select(nfds, &changed_set, NULL, NULL, NULL));
+        if (FD_ISSET(ptym, &changed_set)) {
+            if ((nread = read(ptym, buf, BUFFSIZE)) <= 0)
+                break; // EOF or child exited so and terminal destroyed?
+            if (writen(STDOUT_FILENO, buf, nread) != nread)
+                err_sys("writen error to stdout");
+        }
+        if (FD_ISSET(STDIN_FILENO, &changed_set)) {
             if ((nread = read(STDIN_FILENO, buf, BUFFSIZE)) < 0)
                 err_sys("read error from stdin");
             else if (nread == 0)
-                break;      /* EOF on stdin means we're done */
+                break;
             if (writen(ptym, buf, nread) != nread)
                 err_sys("writen error to master pty");
         }
-
-        /*
-         * We always terminate when we encounter an EOF on stdin,
-         * but we notify the parent only if ignoreeof is 0.
-         */
-        if (ignoreeof == 0)
-            kill(getppid(), SIGTERM);   /* notify parent */
-        exit(0);    /* and terminate; child can't return */
     }
-
-    /*
-     * Parent copies ptym to stdout.
-     */
-    if (signal_intr(SIGTERM, sig_term) == SIG_ERR)
-        err_sys("signal_intr error for SIGTERM");
-
-    for ( ; ; ) {
-        if ((nread = read(ptym, buf, BUFFSIZE)) <= 0)
-            break;      /* signal caught, error, or EOF */
-        if (writen(STDOUT_FILENO, buf, nread) != nread)
-            err_sys("writen error to stdout");
-    }
-
-    /*
-     * There are three ways to get here: sig_term() below caught the
-     * SIGTERM from the child, we read an EOF on the pty master (which
-     * means we have to signal the child to stop), or an error.
-     */
-    if (sigcaught == 0) /* tell child if it didn't send us the signal */
-        kill(child, SIGTERM);
-
-    /*
-     * Parent returns to caller.
-     */
-}
-
-/*
- * The child sends us SIGTERM when it gets EOF on the pty slave or
- * when read() fails.  We probably interrupted the read() of ptym.
- */
-static void
-sig_term(int signo)
-{
-    sigcaught = 1;      /* just set flag and return */
 }
