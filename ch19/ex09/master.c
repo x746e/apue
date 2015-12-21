8420d27b5ee721de1969e27c763d37b6590639f1
Kirill Spitsyn <kyrylo@fb.com>
2015-12-20 18:39:13 -0800

    [ex19.09] Forget to add one file.

---
+++ ch19/ex09/master.c
@@ -0,0 +1,52 @@
+#include "apue.h"
+#include "common.h"
+
+
+#define MAX_NAME_LEN 128
+#define BUFSZ 128
+
+
+int ptym_fd;
+
+
+void slrd() {
+    int n;
+    char buf[BUFSZ] = { 0 };
+    sleep(1);
+    sys_chk(n = read(ptym_fd, buf, BUFSZ));
+    buf[n] = '\0';
+    printf("got from child: %s\n", buf);
+}
+
+
+int main() {
+    pid_t pid;
+    char slave_name[MAX_NAME_LEN];
+    struct winsize size = {
+        3, 4
+    };
+
+    sys_chk(pid = pty_fork(&ptym_fd, slave_name, MAX_NAME_LEN, NULL, NULL));
+
+    if (pid == 0) {  // Child.
+        execl("slave", "slave", (char*) 0);
+        return EXIT_SUCCESS;
+
+    } else {  // Parent.
+        printf("hello from parent\n");
+
+        slrd();
+
+        // send sigterm
+        // doesn't work, failing with EINVAL
+        //int signum = SIGTERM;
+        //ioctl(ptym_fd, TIOCSIG, signum);
+        //slrd();
+        
+        // change window size
+        sys_chk(ioctl(ptym_fd, TIOCSWINSZ, &size));
+        slrd();
+
+        return EXIT_SUCCESS;
+    }
+}
